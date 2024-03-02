c5d2057a770b532a97920a51c332a03dd062967c
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-02 15:13:34 +0600

    show data not found

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const btnContainer = document.getElementById('btn-container');
 const cardContainer = document.getElementById('card-container');
+const errorElement = document.getElementById('error-element');
 let selectedCategory = 1000;
 
 
@@ -14,6 +15,7 @@ const loadFetchCategories = async () => {
 const displayAllCategories = (data) => {
     // console.log(data);
     data.forEach(card => {
+
         // console.log(card);
         const newButton = document.createElement('button');
         newButton.classList = `btn  btn-ghost bg-slate-700 text-white text-lg`;
@@ -34,28 +36,39 @@ const fetchDataByCategories = async (categoryId) => {
 }
 
 const desplayFetchDataByCategories = (data) => {
+    if (data.length === 0) {
+        errorElement.classList.remove('hidden');
+    } else {
+        errorElement.classList.add('hidden')
+    }
     cardContainer.innerHTML = ``;
     data.forEach(video => {
+        // verified batch
+        let verifiedBatch = '';
+        if (video.authors[0].verified) {
+            verifiedBatch = `<img class="w-6 h-6" src="./images/verify.png" alt="">`
+        }
+        // verified batch end
         console.log(video);
         const newCard = document.createElement('div');
         newCard.innerHTML = `
         <div class="card w-full bg-base-100 shadow-xl">
                 <figure class="overflow-hidden h-72">
-                    <img class="w-full" src="./images/smells.jpg" alt="Shoes" />
+                    <img class="w-full" src=${video.thumbnail} />
                     <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
                 </figure>
                 <div class="card-body">
                     <div class="flex space-x-4 justify-start items-start">
                         <div>
-                            <img class="w-12 h-12 rounded-full" src="./images/smells.jpg" alt="Shoes" />
+                            <img class="w-12 h-12 rounded-full" src=${video.authors[0].profile_picture} />
                         </div>
                         <div>
-                            <h2 class="card-title">Laugh At My Pain</h2>
+                            <h2 class="card-title">${video.title}</h2>
                             <div class="flex mt-3">
-                                <p class="">Author Name</p>
-                                <img class="w-6 h-6" src="./images/verify.png" alt="">
+                                <p class="">${video.authors[0].profile_name}</p>
+                                ${verifiedBatch}
                             </div>
-                            <p class="mt-3">100k Views</p>
+                            <p class="mt-3">${video.others.views} </p>
                         </div>
                     </div>
                 </div>
