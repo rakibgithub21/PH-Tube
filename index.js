d90e4bf3fd079eb96bf0584322ff85fa1a8df02c
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-02 14:56:17 +0600

    show data dyunamically

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const btnContainer = document.getElementById('btn-container');
+const cardContainer = document.getElementById('card-container');
+let selectedCategory = 1000;
+
 
 const loadFetchCategories = async () => {
     const url = `https://openapi.programming-hero.com/api/videos/categories`;
@@ -11,19 +14,58 @@ const loadFetchCategories = async () => {
 const displayAllCategories = (data) => {
     // console.log(data);
     data.forEach(card => {
-        console.log(card);
+        // console.log(card);
         const newButton = document.createElement('button');
         newButton.classList = `btn  btn-ghost bg-slate-700 text-white text-lg`;
         newButton.innerText = card.category;
-        newButton.addEventListener('click',()=>fetchDataByCategories(card.category_id))
+        newButton.addEventListener('click', () => fetchDataByCategories(card.category_id))
         btnContainer.appendChild(newButton);
     });
 }
 
 
-const fetchDataByCategories = (categoryId) => {
-    console.log(categoryId);
+const fetchDataByCategories = async (categoryId) => {
+    selectedCategory = categoryId;
+    const url = `https://openapi.programming-hero.com/api/videos/category/${categoryId}`;
+    console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    desplayFetchDataByCategories(data.data);
+}
+
+const desplayFetchDataByCategories = (data) => {
+    cardContainer.innerHTML = ``;
+    data.forEach(video => {
+        console.log(video);
+        const newCard = document.createElement('div');
+        newCard.innerHTML = `
+        <div class="card w-full bg-base-100 shadow-xl">
+                <figure class="overflow-hidden h-72">
+                    <img class="w-full" src="./images/smells.jpg" alt="Shoes" />
+                    <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
+                </figure>
+                <div class="card-body">
+                    <div class="flex space-x-4 justify-start items-start">
+                        <div>
+                            <img class="w-12 h-12 rounded-full" src="./images/smells.jpg" alt="Shoes" />
+                        </div>
+                        <div>
+                            <h2 class="card-title">Laugh At My Pain</h2>
+                            <div class="flex mt-3">
+                                <p class="">Author Name</p>
+                                <img class="w-6 h-6" src="./images/verify.png" alt="">
+                            </div>
+                            <p class="mt-3">100k Views</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        cardContainer.appendChild(newCard)
+
+    });
 }
 
 
-loadFetchCategories()
+loadFetchCategories();
+fetchDataByCategories(selectedCategory);
